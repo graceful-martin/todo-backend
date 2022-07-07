a39dedd35fe9f8f70e453c892aac932c988ed4a7
ssavassava <sangwo98.kang@gmail.com>
2022-07-07 22:19:24 +0900

    feature : TodoRepository의 findByUserId 추가 MethodInterceptor AOP 인터페이스를 사용하여 JpaRepository의 메서드를 호출하면 메서드 콜을 가로채며 DB에서 userId로 TodoEntity List를 찾는 메서드 구현

--- src/main/java/com/example/demo/persistence/TodoRepository.java
+++ src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -2,9 +2,12 @@ package com.example.demo.persistence;
 
 import org.springframework.stereotype.Repository;
 import com.example.demo.model.TodoEntity;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
-
+	List<TodoEntity> findByUserId(String userId);
 }
